24ecba2423e1c83134705a1c2fbe857e00b8e0ec
kate_lulu <lukoszek.katarzyna@gmail.com>
2022-03-24 17:56:56 +0100

    icon

--- src/Components/Sidebar/Sidebar.js
+++ src/Components/Sidebar/Sidebar.js
@@ -1,35 +1,29 @@
-import React from 'react';
-import './Sidebar.css'
-import { SidebarData } from './SideMenuData';
-import { Navbar, Nav, Container } from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-
-
+import React from "react"
+import "./Sidebar.css"
+import { SidebarData } from "./SideMenuData"
+import { Navbar, Nav, Container } from "react-bootstrap"
+import "bootstrap/dist/css/bootstrap.min.css"
 
 export default function Sidebar() {
-
-    return (
-        <Navbar collapseOnSelect expand="lg">
-            <Container className="sidemenu">
-                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                <Navbar.Collapse id="responsive-navbar-nav">
-                    <Nav className="me-auto flex-column">
-                        {SidebarData.map((item, index) => {
-                            return (
-                                <li key={index} className={item.cName}>
-                                    <Nav.Link to={item.path}>
-                                        {item.icon}
-                                        <span>{item.title}</span>
-                                    </Nav.Link>
-                                </li>
-                            )
-                        })}
-                    </Nav>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
-    )
+  return (
+    <Navbar collapseOnSelect expand="lg">
+      <Container className="sidemenu">
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto flex-column">
+            {SidebarData.map((item, index) => {
+              return (
+                <li key={index} className={item.cName}>
+                  <Nav.Link to={item.path}>
+                    {item.icon}
+                    <span>{item.title}</span>
+                  </Nav.Link>
+                </li>
+              )
+            })}
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  )
 }
-
-
